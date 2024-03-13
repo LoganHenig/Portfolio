556005ecbdcdc1561be71b38c8410a0aeb154910
lhenig <107576876+lhenig@users.noreply.github.com>
2024-03-13 13:54:46 -0400

    made skills dynamic and added btn css

--- 2024-portfolio/src/components/Landing.js
+++ 2024-portfolio/src/components/Landing.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { Fade } from "react-awesome-reveal";
 import { CustomCard } from "./Card";
 import {NavBar} from "./NavBar"
+import {Chip} from "@nextui-org/react";
+import {CircularProgress} from "@nextui-org/react";
 
 import reactIcon from '../assets/reactIcon.png';
 import nextUIIcon from '../assets/nextUI.png';
@@ -24,27 +26,67 @@ import { MyDivider } from "./divider";
 
 
 export const Landing = () => {
+
+    const [languageChip, setLanguageChip] = useState(false);
+    const [uiChip, setUiChip] = useState(false);
+    const [database, setDatabase] = useState(false);
+
+    const [delayBool, setDelayBool] = useState(true);
+
+    const [filteredCardData, setFilteredCardData] = useState([]);
     
     const cardData = [
-        <CustomCard  iconSrc={reactIcon} bodyText='ReactJS is a free and open-source front-end JavaScript library for building user interfaces based on components' link='https://react.dev/' headerText='React' headerUrl='react.dev' linkText='View Documentation'/>,
-        <CustomCard  iconSrc={nextUIIcon} bodyText='NextUI is a component-based CSS framework, which means it provides a set of pre-designed components you can use at any time.' link='https://nextui.org/' headerText='NextUI' headerUrl='nextui.org' linkText='View Documentation'/>,
-        <CustomCard  iconSrc={reveal} bodyText='React Awesome Reveal is a react library that uses Web Animations API for performant animations.' link='https://react-awesome-reveal.morello.dev/' headerText='React Awesom Reveal' headerUrl='react-awesome-reveal.morello.dev' linkText='View Documentation'/>,
-        <CustomCard  iconSrc={vue} bodyText='Vue.js is an open-source model-view-viewmodel front end JavaScript library for building user interfaces and single-page applications.' link='https://vuejs.org/' headerText='Vue' headerUrl='vuejs.org/' linkText='View Documentation'/>,
-        <CustomCard  iconSrc={js} bodyText='JavaScript is a scripting language that enables you to create dynamically updating content, control multimedia, animate images, and pretty much everything else.' link='https://developer.oracle.com/languages/javascript.html' headerText='JavaScript' headerUrl='developer.oracle.com' linkText='View Documentation'/>,
-        <CustomCard  iconSrc={github} bodyText='GitHub is a developer platform that allows developers to create, store, manage and share their code.' link='https://github.com/' headerText='GitHub' headerUrl='github.com' linkText='View Documentation'/>,
-        <CustomCard  iconSrc={spring} bodyText='Spring Boot is an open-source Java-based framework used for creating standalone, production-grade Spring-based applications with minimal effort.' link='https://spring.io/projects/spring-boot' headerText='Spring Boot' headerUrl='spring.io' linkText='View Documentation'/>,
-        <CustomCard  iconSrc={mongo} bodyText='MongoDB is a source-available, cross-platform, document-oriented database program. Classified as a NoSQL database product. ' link='https://www.mongodb.com/' headerText='MongoDB' headerUrl='mongodb.com' linkText='View Documentation'/>,
-        <CustomCard  iconSrc={azure} bodyText='Microsoft Azure, is a cloud computing platform run by Microsoft. It offers access, management, and the development of applications and services through global data centers.' link='https://azure.microsoft.com/' headerText='Azure Dev Ops' headerUrl='/azure.microsoft.com' linkText='View Documentation'/>,
-        <CustomCard  iconSrc={java} bodyText='Java is a high-level, class-based, object-oriented programming language that is designed with few dependencies and a write once, run anywhere architecture.' link='https://www.java.com/en/' headerText='Java' headerUrl='java.com' linkText='View Documentation'/>,
-        <CustomCard  iconSrc={cpp} bodyText='C++ is a cross-platform language that can be used to create high-performance applications. C++ gives programmers control over system resources and memory.' link='https://www.w3schools.com/cpp/default.asp' headerText='C++' headerUrl='w3schools.com' linkText='View Documentation'/>,
-        <CustomCard  iconSrc={python} bodyText='Python is a high-level, general-purpose programming language. Its design philosophy emphasizes code readability with the use of significant indentation.' link='https://www.python.org/doc/' headerText='Python' headerUrl='python.org' linkText='View Documentation'/>,
-        <CustomCard  iconSrc={sql} bodyText='Structured Query Language is a domain-specific language used to manage data, especially in a relational database management system.' link='https://www.w3schools.com/sql' headerText='SQL' headerUrl='w3schools.com/sql' linkText='View Documentation'/>,
-        <CustomCard  iconSrc={html} bodyText='HyperText Markup Language or HTML is the standard markup language for documents designed to be displayed in a web browser. It defines the content and structure of web content.' link='https://www.w3schools.com/html/' headerText='HTML' headerUrl='w3schools.com/html' linkText='View Documentation'/>,
-        <CustomCard  iconSrc={axios} bodyText='Axios is a promise-based HTTP client that works both in the browser and in a Node.js environment. It provides a single API for dealing with XMLHttpRequests and node’s http interface.' link='https://axios-http.com/docs' headerText='Axios' headerUrl='axios-http.com' linkText='View Documentation'/>,
-        // <CustomCard  iconSrc={} bodyText='' link='' headerText='' headerUrl='' linkText='View Documentation'/>,
-        
-
+        <CustomCard tag="UI"    iconSrc={reactIcon} bodyText='ReactJS is a free and open-source front-end JavaScript library for building user interfaces based on components' link='https://react.dev/' headerText='React' headerUrl='react.dev' linkText='View Documentation'/>,
+        <CustomCard tag="UI"    iconSrc={nextUIIcon} bodyText='NextUI is a component-based CSS framework, which means it provides a set of pre-designed components you can use at any time.' link='https://nextui.org/' headerText='NextUI' headerUrl='nextui.org' linkText='View Documentation'/>,
+        <CustomCard tag="UI"    iconSrc={reveal} bodyText='React Awesome Reveal is a react library that uses Web Animations API for performant animations.' link='https://react-awesome-reveal.morello.dev/' headerText='React Awesom Reveal' headerUrl='react-awesome-reveal.morello.dev' linkText='View Documentation'/>,
+        <CustomCard tag="UI"    iconSrc={vue} bodyText='Vue.js is an open-source model-view-viewmodel front end JavaScript library for building user interfaces and single-page applications.' link='https://vuejs.org/' headerText='Vue' headerUrl='vuejs.org' linkText='View Documentation'/>,
+        <CustomCard tag="Lang"  iconSrc={js} bodyText='JavaScript is a scripting language that enables you to create dynamically updating content, control multimedia, animate images, and pretty much everything else.' link='https://developer.oracle.com/languages/javascript.html' headerText='JavaScript' headerUrl='developer.oracle.com' linkText='View Documentation'/>,
+        <CustomCard tag="Other" iconSrc={github} bodyText='GitHub is a developer platform that allows developers to create, store, manage and share their code.' link='https://github.com/' headerText='GitHub' headerUrl='github.com' linkText='View Documentation'/>,
+        <CustomCard tag="Data"  iconSrc={spring} bodyText='Spring Boot is an open-source Java-based framework used for creating standalone, production-grade Spring-based applications with minimal effort.' link='https://spring.io/projects/spring-boot' headerText='Spring Boot' headerUrl='spring.io' linkText='View Documentation'/>,
+        <CustomCard tag="Data"  iconSrc={mongo} bodyText='MongoDB is a source-available, cross-platform, document-oriented database program. Classified as a NoSQL database product. ' link='https://www.mongodb.com/' headerText='MongoDB' headerUrl='mongodb.com' linkText='View Documentation'/>,
+        <CustomCard tag="Other" iconSrc={azure} bodyText='Microsoft Azure, is a cloud computing platform run by Microsoft. It offers access, management, and the development of applications and services through global data centers.' link='https://azure.microsoft.com/' headerText='Azure Dev Ops' headerUrl='/azure.microsoft.com' linkText='View Documentation'/>,
+        <CustomCard tag="Lang"  iconSrc={java} bodyText='Java is a high-level, class-based, object-oriented programming language that is designed with few dependencies and a write once, run anywhere architecture.' link='https://www.java.com/en/' headerText='Java' headerUrl='java.com' linkText='View Documentation'/>,
+        <CustomCard tag="Lang"  iconSrc={cpp} bodyText='C++ is a cross-platform language that can be used to create high-performance applications. C++ gives programmers control over system resources and memory.' link='https://www.w3schools.com/cpp/default.asp' headerText='C++' headerUrl='w3schools.com' linkText='View Documentation'/>,
+        <CustomCard tag="Lang"  iconSrc={python} bodyText='Python is a high-level, general-purpose programming language. Its design philosophy emphasizes code readability with the use of significant indentation.' link='https://www.python.org/doc/' headerText='Python' headerUrl='python.org' linkText='View Documentation'/>,
+        <CustomCard tag="Data"  iconSrc={sql} bodyText='Structured Query Language is a domain-specific language used to manage data, especially in a relational database management system.' link='https://www.w3schools.com/sql' headerText='SQL' headerUrl='w3schools.com/sql' linkText='View Documentation'/>,
+        <CustomCard tag="Lang"  iconSrc={html} bodyText='HyperText Markup Language or HTML is the standard markup language for documents designed to be displayed in a web browser. It defines the content and structure of web content.' link='https://www.w3schools.com/html/' headerText='HTML' headerUrl='w3schools.com/html' linkText='View Documentation'/>,
+        <CustomCard tag="UI"    iconSrc={axios} bodyText='Axios is a promise-based HTTP client that works both in the browser and in a Node.js environment. It provides a single API for dealing with XMLHttpRequests and node’s http interface.' link='https://axios-http.com/docs' headerText='Axios' headerUrl='axios-http.com' linkText='View Documentation'/>,
+        // <CustomCard  iconSrc={} bodyText='' link='' headerText='' headerUrl='' linkText='View Documentation'/>,  
     ]
+    useEffect(() =>{    
+        let tempCards = [];
+        if(languageChip){
+            tempCards = [...tempCards, cardData.filter((card) => card.props.tag ==="Lang")];
+            console.log(tempCards)
+        } 
+        if(database){
+            tempCards = [...tempCards, cardData.filter((card) => card.props.tag ==="Data")];
+            console.log(tempCards)
+        } 
+        if(uiChip){
+            tempCards = [...tempCards, cardData.filter((card) => card.props.tag ==="UI")];
+            console.log(tempCards)
+        } 
+        if(!uiChip && !languageChip && !database){
+            tempCards = cardData
+        }
+        setFilteredCardData(tempCards)
+    },[languageChip, database, uiChip]);
+
+    useEffect(() =>{
+        console.log(delayBool)
+    },[delayBool])
+
+
+    const delay = ms => new Promise(
+        resolve => setTimeout(resolve, ms)
+    );
+    const clearCards = (async (time) =>{
+        setDelayBool(false);
+        await delay(time)
+        setDelayBool(true);
+    })
+
 
   return (
     <main className="dark text-foreground bg-background fill-screen">
@@ -75,13 +117,27 @@ export const Landing = () => {
             
         </Fade>
     </div>    
-    <h3 className='technology-used'> Technology I have used to make projects </h3>
+    <h3 className='technology-used'> Technology I have used to make other projects </h3>
+    <div className="chip-container">
+        <div className="flex gap-4 flex-start">
+
+            <Chip className="chip" onClick={() => {setLanguageChip(!languageChip); clearCards(1000);}} color={`${languageChip ? "primary" : "default"}`}>Language</Chip>
+            <Chip className="chip" onClick={() => {setUiChip(!uiChip); clearCards(1000);}} color={`${uiChip ? "primary" : "default"}`}>User Interface</Chip>
+            <Chip className="chip" onClick={() => {setDatabase(!database); clearCards(1000);}} color={`${database ? "primary" : "default"}`}>Backend/Database</Chip>
+        </div> 
+    </div>
     <div className="tech-continer" >
+
+        {delayBool && 
         <Fade cascade={true} damping={0.1}>
-            
-        {cardData}
-            
+        {filteredCardData} 
         </Fade>
+        }
+        {!delayBool &&
+        <div className="loading">
+        <CircularProgress aria-label="Loading..." />
+        </div>
+        }
     </div> 
     </div>
     <Footer/>
--- 2024-portfolio/src/components/divider.js
+++ 2024-portfolio/src/components/divider.js
@@ -1,6 +1,7 @@
 import React, {useState}from "react";
 import {Divider} from "@nextui-org/react";
 import './divider.css'
+import { Fade } from "react-awesome-reveal";
 
 
 export const MyDivider = ({number, hidden}) => {
@@ -44,20 +45,33 @@ export const MyDivider = ({number, hidden}) => {
     </div>
     {college &&
     <div className="highlights-text">
-    <div>Cedarville University,</div>
-    During my time at college, I dove into the world of computer science with a focus on Cyber Operations. I got my hands dirty with Object-Oriented Programming (OOP), Dynamic Programming, Threading, and lots more. Beyond the classroom, I immersed myself in computer-related clubs like the Cyber Defense Club, where I learned practical skills and soaked up knowledge like a sponge. Along the way, I also delved into databases and web applications, gaining valuable insights that continue to shape my journey as a web developer. Cedarville University was more than just a school; it was where my passion for technology truly flourished, setting me on the path to becoming the developer I am today.
+        <Fade>
+            <div>Cedarville University,</div>
+            <div>
+            During my time at college, I dove into the world of computer science with a focus on Cyber Operations. I got my hands dirty with Object-Oriented Programming (OOP), Dynamic Programming, Threading, and lots more. Beyond the classroom, I immersed myself in computer-related clubs like the Cyber Defense Club, where I learned practical skills and soaked up knowledge like a sponge. Along the way, I also delved into databases and web applications, gaining valuable insights that continue to shape my journey as a web developer. Cedarville University was more than just a school; it was where my passion for technology truly flourished, setting me on the path to becoming the developer I am today.
+            </div>
+        </Fade>
     </div>
+   
     }
     {cert &&
     <div className="highlights-text">
-    <div>Certifications,</div>
-    During my intensive four-month training at Skillstorm, I achieved significant milestones in my professional development. Notably, I obtained the Java Foundations certification by successfully passing the Java Foundations | 1Z0-811 exam. This certification not only validated my comprehensive understanding of Java but also highlighted my proficiency in foundational Java concepts. Additionally, I dedicated myself to mastering cybersecurity principles, a commitment evidenced by my successful completion of the Security+ exam, resulting in my attainment of the CompTIA Security+ ce certification. These accomplishments not only signify my expertise in Java programming and cybersecurity but also demonstrate my relentless pursuit of excellence and readiness to tackle complex challenges in the ever-evolving tech landscape.
+        <Fade>
+            <div>Certifications,</div>
+            <div>
+                During my intensive four-month training at Skillstorm, I achieved significant milestones in my professional development. Notably, I obtained the Java Foundations certification by successfully passing the Java Foundations | 1Z0-811 exam. This certification not only validated my comprehensive understanding of Java but also highlighted my proficiency in foundational Java concepts. Additionally, I dedicated myself to mastering cybersecurity principles, a commitment evidenced by my successful completion of the Security+ exam, resulting in my attainment of the CompTIA Security+ ce certification. These accomplishments not only signify my expertise in Java programming and cybersecurity but also demonstrate my relentless pursuit of excellence and readiness to tackle complex challenges in the ever-evolving tech landscape.
+            </div>
+        </Fade>
     </div>
     }
     {job &&
     <div className="highlights-text">
-    <div>SkillStorm,</div>
-    Currently, I am employed at Skillstorm, where I am contracted to work with Price Waterhouse Coopers (PwC). Over the past year, my team and I have embarked on an exciting journey, building from scratch a cutting-edge React web application and a robust Spring Boot backend. Together, these components seamlessly interact to drastically improve the user experience for technology registration at PwC. It's been a thrilling process witnessing our project evolve from conceptualization to implementation, and I take pride in our collective effort to deliver a solution that not only meets but exceeds expectations. Our collaboration underscores the power of teamwork and innovation in creating transformative solutions for real-world challenges.
+        <Fade>
+            <div>SkillStorm,</div>
+            <div>
+            Currently, I am employed at Skillstorm, where I am contracted to work with Price Waterhouse Coopers (PwC). Over the past year, my team and I have embarked on an exciting journey, building from scratch a cutting-edge React web application and a robust Spring Boot backend. Together, these components seamlessly interact to drastically improve the user experience for technology registration at PwC. It's been a thrilling process witnessing our project evolve from conceptualization to implementation, and I take pride in our collective effort to deliver a solution that not only meets but exceeds expectations. Our collaboration underscores the power of teamwork and innovation in creating transformative solutions for real-world challenges.
+            </div>
+        </Fade>
     </div>
     }
     </>
